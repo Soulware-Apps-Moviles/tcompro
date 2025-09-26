18aabed9ba3795906ff61e5e9376efcf3d678b4d
Henry Reaño <henry29.2004@hotmail.com>
2025-09-26 17:13:57 -0500

    feat: add order context domain

---
+++ src/main/java/com/soulware/tcompro/order/domain/model/entities/OrderStatus.java
@@ -0,0 +1,23 @@
+package com.soulware.tcompro.order.domain.model.entities;
+
+import com.soulware.tcompro.order.domain.model.valueobjects.OrderStatusId;
+import com.soulware.tcompro.order.domain.model.valueobjects.OrderStatuses;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "order_statuses")
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderStatus {
+
+    @EmbeddedId
+    private OrderStatusId id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private OrderStatuses name;
+}
---
+++ src/main/java/com/soulware/tcompro/order/domain/model/valueobjects/OrderStatuses.java
@@ -0,0 +1,12 @@
+package com.soulware.tcompro.order.domain.model.valueobjects;
+
+public enum OrderStatuses {
+    PLACED,
+    ACCEPTED,
+    REJECTED,
+    CANCELED,
+    READY,
+    PICKED_UP,
+    DISPATCHED,
+    DELIVERED
+}
